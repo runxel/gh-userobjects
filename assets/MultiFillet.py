e0c4f3ec79da0501af63c2a2909b8db3fb328a0f
Lucas Becker <runxel@users.noreply.github.com>
2019-09-16 16:36:23 +0200

    Update MultiFillet.py

--- assets/MultiFillet.py
+++ assets/MultiFillet.py
@@ -1,35 +1,56 @@
 """
-Provides fillets where the output is one final curve.
+Provides fillets for curves where the output is one final curve.
 
     Args:
-        crv: The curve to fillet. {curve}
-        crv_t: Params of 'crv' where it shall fillet. {float | List access}
-        radius: The radius of the fillet. {float}
+        crv: The curve to fillet.
+        crv_t: Params of 'crv' where it shall fillet.
+        radius: The radius (or radii) of the fillets.
     Returns:
-        fcrv: The filleted curve. 
+        FC: The filleted curve. 
 """
 
 __author__ = "Lucas Becker @runxel"
-__version__ = "2019-07-08"
+__version__ = "2019-09-16"
 
 import rhinoscriptsyntax as rs
 import Rhino.Geometry as rg
 from scriptcontext import doc
 
-if not radius:
-    radius = doc.ModelAbsoluteTolerance * 10
+
+n_f = 0  # for better help when it fails
+n_radius = len(radius)
+n_crvparam = len(crv_t)
+
+if (n_radius != n_crvparam) and (n_radius > 1):
+    diff_in_size = abs(n_radius - n_crvparam)
+    if n_radius < n_crvparam:
+        add_error_text = "{} too few radii provided.".format(diff_in_size)
+    else:
+        add_error_text = "{} too many radii provided.".format(diff_in_size)
+
+    raise RuntimeError("\nMismatch of curve params and radii.\n" + add_error_text)
+
+
+def shift(l, n):
+    return l[n:] + l[:n]
 
 def fillet(crv1, crv2, radius):
     """ Wrapper for Curve.CreateFilletCurves Method """
+    global n_f
     # reparam first
     crv1.Domain = rg.Interval(0,1)
     crv2.Domain = rg.Interval(0,1)
-    pt1 = crv1.PointAt(0.9)
-    pt2 = crv2.PointAt(0.1)
+    pt1 = crv1.PointAt(0.999)
+    pt2 = crv2.PointAt(0.001)
 
     fillet_crv = rg.Curve.CreateFilletCurves(crv1, pt1, crv2, pt2,
                                              radius, True, True, True,
                                              doc.ModelAbsoluteTolerance, doc.ModelAngleToleranceDegrees)
+
+    if len(fillet_crv) == 0:
+        raise RuntimeError("\nRadius is too big.\nMost possible culprit is [" + str(n_f) + "].")
+    n_f += 1
+
     return fillet_crv[0]  # otherwise we would return an array
 
 
@@ -38,14 +59,19 @@ closed = crv.IsClosed
 # split the curve at the provided params
 splitted = crv.Split(crv_t)
 # init the interim result
+if closed:
+    splitted = shift(splitted, -1)
 interim = splitted[0]
 
 for _, i in enumerate(range(len(splitted)-1)):
     # interim will grow until only the last fillet (with itself) remains
-    interim = fillet(interim, splitted[i+1], radius)
+    if n_radius > 1:
+        interim = fillet(interim, splitted[i+1], radius[i])
+    else:
+        interim = fillet(interim, splitted[i+1], radius[0])
 
 # final result:
 if closed:  # then also do the last fillet with itself
-    fcrv = fillet(interim, interim, radius)
+    FC = fillet(interim, interim, radius[-1])
 else:       # otherwise we're already done
-    fcrv = interim
+    FC = interim
